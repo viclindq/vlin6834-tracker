987f43fd05b593c8f12598062854554458c7f42b
viclindq <victoria@linton.fi>
2024-06-07 18:36:20 +1000

    Work before bootstrap

--- public/static/js/views/Dashboard.js
+++ public/static/js/views/Dashboard.js
@@ -8,13 +8,14 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Welcome back, Dom</h1>
+            <h1>Your Top Genre: Jazz House</h1>
             <p>
                 Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
             </p>
             <p>
                 <a href="/posts" data-link>View recent posts</a>.
             </p>
+
         `;
     }
 }
