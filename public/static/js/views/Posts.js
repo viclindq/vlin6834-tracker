0a0f4c669f74a64174f537f739190eef7a7f1a69
viclindq <victoria@linton.fi>
2024-06-08 20:23:27 +1000

    Recent Songs You Added functionality without descending order from date

--- public/static/js/views/Posts.js
+++ public/static/js/views/Posts.js
@@ -27,62 +27,39 @@ export default class extends AbstractView {
             <div class="row mt-2"> <!-- Adjusted margin-top -->
                 <h2>Recent Songs You Added</h2>
             </div>
-            <div class="row gy-4">
-                <div class="col-md-2">
-                    <div class="bg-grey rounded p-3 text-white h-100 d-flex flex-column min-height">
-                        <img src="https://www.udiscovermusic.com/wp-content/uploads/2017/08/Pink-Floyd-Dark-Side-Of-The-Moon.jpg" alt="Album Cover 1">
-                        <p class="song-name">Song Name 1</p>
-                        <p class="artist">Artist Name 1</p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="bg-grey rounded p-3 text-white h-100 d-flex flex-column min-height">
-                        <img src="https://assets-global.website-files.com/5e6a544cadf84b1393e2e022/611cfe2fe8dfe7fe77ba50c4_cri_000000319870%20(1).jpeg" alt="Album Cover 2">
-                        <p class="song-name">Song Name 2</p>
-                        <p class="artist">Artist Name 2</p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="bg-grey rounded p-3 text-white h-100 d-flex flex-column min-height">
-                        <img src="https://www.billboard.com/wp-content/uploads/2023/07/rihanna-anti-cover-2016-billboard-1240.jpg?w=600" alt="Album Cover 3">
-                        <p class="song-name">Song Name 3</p>
-                        <p class="artist">Artist Name 3</p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="bg-grey rounded p-3 text-white h-100 d-flex flex-column min-height">
-                        <img src="https://hips.hearstapps.com/hmg-prod/images/7-64ecb1c909b78.png?crop=0.502xw:1.00xh;0.498xw,0&resize=1200:*" alt="Album Cover 4">
-                        <p class="song-name">Song Name 4</p>
-                        <p class="artist">Artist Name 4</p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="bg-grey rounded p-3 text-white h-100 d-flex flex-column min-height">
-                        <img src="https://www.fubiz.net/wp-content/uploads/2018/01/01-kendrick-lamar-damn-album-art-2017-billboard-1240.jpg" alt="Album Cover 5">
-                        <p class="song-name">Song Name 5</p>
-                        <p class="artist">Artist Name 5</p>
-                    </div>
+            <div class="container-fluid bg-grey rounded p-3 text-white mt-4 recent-songs-container" id="recent-songs-container">
+                <div class="row gy-4">
+                    <!-- Recent songs will be loaded here -->
                 </div>
             </div>
-            
-            
         `;
     }
 
     async loadFavSongs() {
         console.log("I am loading saved songs");
         const savedSongs = JSON.parse(localStorage.getItem('savedSongs'));
-        const container = document.getElementById('favorite-songs-container');
+        const favoriteContainer = document.getElementById('favorite-songs-container');
+        const recentContainer = document.querySelector('#recent-songs-container .row');
 
         if (!savedSongs || savedSongs.length === 0) {
-            container.innerHTML = `<p>No favorite songs added yet.</p>`;
+            favoriteContainer.innerHTML = `<p>No favorite songs added yet.</p>`;
+            recentContainer.innerHTML = `<p>No recent songs added yet.</p>`;
             return;
         }
 
-        container.innerHTML = savedSongs.map(song => this.generateSongHTML(song)).join('');
+        favoriteContainer.innerHTML = savedSongs.map(song => this.generateFavoriteSongHTML(song)).join('');
+
+        // Sort the songs by date added in descending order
+        savedSongs.sort((a, b) => new Date(JSON.parse(b).addedTime) - new Date(JSON.parse(a).addedTime));
+
+        // Take the top 5 most recently added songs
+        const recentSongs = savedSongs.slice(0, 5);
+
+        // Generate HTML for each recent song and populate the container
+        recentContainer.innerHTML = recentSongs.map(song => this.generateRecentSongHTML(song)).join('');
     }
 
-    generateSongHTML(song) {
+    generateFavoriteSongHTML(song) {
         const { title, artist, genre, duration, album } = JSON.parse(song);
         return `
             <div class="row song-row">
@@ -92,6 +69,19 @@ export default class extends AbstractView {
                 <div class="col" style="font-size: 14px;">${genre}</div>
                 <div class="col" style="font-size: 14px;">${new Date().toLocaleDateString()}</div>
                 <div class="col" style="font-size: 14px;">${Math.floor(duration / 60)}:${duration % 60}</div>
+            </div>
+        `;
+    }
+
+    generateRecentSongHTML(song) {
+        const { title, artist, album } = JSON.parse(song);
+        return `
+            <div class="col-md-2">
+                <div class="bg-light-grey rounded p-3 text-white h-100 d-flex flex-column min-height">
+                    <img src="${album.cover}" alt="Album Cover" class="album-cover">
+                    <p class="song-name">${title}</p>
+                    <p class="artist">${artist.name}</p>
+                </div>
             </div>
         `;
     }
